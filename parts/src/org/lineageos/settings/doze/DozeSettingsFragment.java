8e6c7ca31dbdf2b8f52a547b8733496b1615f2cb
Nauval Rizky <enuma.alrizky@gmail.com>
2020-01-31 11:57:48 +0900

    begonia: parts: Introduce raise to wake gesture
    * Highly inspired by iOS raise to wake
    * Add min interval for raise2wake
    * Check proximity before wakeup
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: David Setiawan <fryevia@foxmail.com>
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>

--- parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
+++ parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
@@ -45,6 +45,7 @@ public class DozeSettingsFragment extends PreferenceFragment implements
     private SwitchPreference mAlwaysOnDisplayPreference;
     private SwitchPreference mWakeOnGesturePreference;
     private SwitchPreference mPickUpPreference;
+    private SwitchPreference mRaiseToWakePreference;
     private SwitchPreference mHandwavePreference;
     private SwitchPreference mPocketPreference;
 
@@ -84,6 +85,10 @@ public class DozeSettingsFragment extends PreferenceFragment implements
         mPickUpPreference.setEnabled(dozeEnabled);
         mPickUpPreference.setOnPreferenceChangeListener(this);
 
+        mRaiseToWakePreference = (SwitchPreference) findPreference(DozeUtils.GESTURE_RAISE_TO_WAKE_KEY);
+        mRaiseToWakePreference.setEnabled(dozeEnabled);
+        mRaiseToWakePreference.setOnPreferenceChangeListener(this);
+
         mHandwavePreference = (SwitchPreference) findPreference(DozeUtils.GESTURE_HAND_WAVE_KEY);
         mHandwavePreference.setEnabled(dozeEnabled);
         mHandwavePreference.setOnPreferenceChangeListener(this);
@@ -104,6 +109,8 @@ public class DozeSettingsFragment extends PreferenceFragment implements
             mWakeOnGesturePreference.setDependency(DozeUtils.ALWAYS_ON_DISPLAY);
             pickupSensorCategory.setDependency(DozeUtils.ALWAYS_ON_DISPLAY);
             proximitySensorCategory.setDependency(DozeUtils.ALWAYS_ON_DISPLAY);
+            mPickUpPreference.setDependency(DozeUtils.GESTURE_RAISE_TO_WAKE_KEY);
+            mPocketPreference.setDependency(DozeUtils.GESTURE_RAISE_TO_WAKE_KEY);
         }
     }
 
@@ -132,6 +139,7 @@ public class DozeSettingsFragment extends PreferenceFragment implements
         mAlwaysOnDisplayPreference.setEnabled(isChecked);
         mWakeOnGesturePreference.setEnabled(isChecked);
         mPickUpPreference.setEnabled(isChecked);
+        mRaiseToWakePreference.setEnabled(isChecked);
         mHandwavePreference.setEnabled(isChecked);
         mPocketPreference.setEnabled(isChecked);
     }
