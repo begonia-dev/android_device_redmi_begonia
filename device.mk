456e296c57bbf3f7d90d010d5523c6cf1671eb79
Kshitij Gupta <kshitijgm@gmail.com>
2020-06-14 21:47:49 +0530

    begonia: Build snap and add camera props
    - Appends extra resolutions and marks ANX as priveleged
    
    Change-Id: Ic09217d1728956f10a6992fb564d3d87aab7a981

--- device.mk
+++ device.mk
@@ -26,6 +26,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/audio/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
 # Dex
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
