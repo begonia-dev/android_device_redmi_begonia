399bac218a9065674f0500c1303d8ad75ee06e77
Aayush Gupta <aayushgupta219@gmail.com>
2020-08-02 07:40:32 +0000

    begonia: Inherit sepolicy for mediatek devices
    There are lot of denials which are not device-specific. Henceforth,
    move such denials to a seperate sepolicy repository and inherit from it.
    
    This reverts commit 4276fbd8e84120eac925b52fd1349c0dd424d33b as well.
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I95c1f35006d49b797c617c5fea5ff27e5f98d45e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,7 +57,7 @@ TARGET_BOARD_PLATFORM := mt6785
 TARGET_BOARD_PLATFORM_GPU := mali-g76mc4
 
 # Kernel
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_KERNEL_PAGESIZE := 2048
@@ -151,10 +151,7 @@ PRODUCT_VENDOR_MOVE_ENABLED := true
 BOARD_VNDK_RUNTIME_DISABLE := true
 
 # Sepolicy
-#include device/mediatek/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-#BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
-#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+include device/mediatek/sepolicy/sepolicy.mk
 
 # Inherit from the proprietary version
 -include vendor/redmi/begonia/BoardConfigVendor.mk
