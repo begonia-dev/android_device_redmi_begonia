26da413d7e5b061ffda2b4350e01bc3b6499ea10
Zinadin Zidan <zidan44@pixelexperience.org>
2021-02-01 22:40:19 +0600

    begonia: Update DRM blobs from from begonia A11 release
    * Also drop old drm entry from manifest.
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Iafab8ca3815fa831d14eb1af388a30f52b00de56
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- device.mk
+++ device.mk
@@ -70,8 +70,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.0-impl:64 \
+    android.hardware.drm@1.0-service-lazy \
     android.hardware.drm@1.3-service.clearkey
 
 # Health
