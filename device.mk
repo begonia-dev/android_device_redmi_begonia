3bff431b42c19933f9d541726d40ffa64cc6b2db
dianlujitao <dianlujitao@lineageos.org>
2021-02-17 23:35:11 +0800

    begonia: Guard with soong namespace
    Change-Id: If98cd52dd06983e77c6f608eeaf1aaae763233a9

--- device.mk
+++ device.mk
@@ -10,6 +10,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # MTK-OSS
 $(call inherit-product, vendor/mediatek/opensource/mtk-oss.mk)
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl \
