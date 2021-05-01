06bbc902547de0c6d7c8b8375fb2b7c14ccd6aa6
TheMalachite <eliasgheeraert@gmail.com>
2021-05-01 12:18:01 +0600

    begonia: Build libtextclassifier hash from source
    * This is a dependencies of MTK 1.3 Neural Networks service
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -249,6 +249,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+# Text classifier
+PRODUCT_PACKAGES += \
+    libtextclassifier_hash.vendor
+
 # TinyXML
 PRODUCT_PACKAGES += \
     libtinyxml
