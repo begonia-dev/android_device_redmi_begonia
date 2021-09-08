5b812e4bdeca9f6feaf8301a9aa178d662953678
Joey <joey@lineageos.org>
2021-09-08 13:48:54 +0200

    begonia: Build Trust HAL

--- device.mk
+++ device.mk
@@ -251,6 +251,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtinyxml
 
+# Trust
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
      android.hardware.vibrator@1.0-impl \
