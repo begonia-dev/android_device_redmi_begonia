b9eeb86cb17207196b8d454657e5554194510be6
theimpulson <aayushgupta219@gmail.com>
2021-09-08 10:26:53 +0200

    begonia: Import LineageOS overlays
    * From: https://github.com/xiaomi-mt6785-devs/android_device_xiaomi_mt6785-common/tree/lineage-18.1/overlay
    
    * Also move Dialer and Snap to LineageOS overlays.
    
    Co-authored-by: Jesse Chan <jc@lineageos.org>
    Co-authored-by: Thomas Shum <jabashque@gmail.com>
    Co-authored-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>
    Co-authored-by: Kshitij Gupta <kshitijgm@gmail.com>

--- device.mk
+++ device.mk
@@ -143,7 +143,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
