1f5d88a11353ce7d9dd7f91c1360b4d255a959b6
DarkJoker360 <simoespo159@gmail.com>
2021-09-08 13:38:43 +0200

    begonia: Update redfin build fingerprint to September 2021
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- lineage_begonia.mk
+++ lineage_begonia.mk
@@ -26,7 +26,7 @@ PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/sunfish/sunfish:11/RQ3A.210805.001.A1/7474174:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="begonia-user 11 RP1A.200720.011 V12.5.1.0.RGGMIXM release-keys" \
