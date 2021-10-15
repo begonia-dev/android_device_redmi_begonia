e63a3e1c35aad2142b70281aabfe1acb71732b15
TheMalachite <eliasgheeraert@gmail.com>
2021-10-15 21:48:50 +0200

    begonia: Drop libhidl
    MediaTek devices doesn't need to build any of those libs
    
    Change-Id: If82104f3dcca3b7bd275b919702708c03b4ffbf5

--- device.mk
+++ device.mk
@@ -92,13 +92,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
-# HIDL
-PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
-
 # Init
 PRODUCT_PACKAGES += \
     init.mtk.rc
