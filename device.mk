6b638368e0f4b162a41b999a3b49d8e06d538582
TheMalachite <eliasgheeraert@gmail.com>
2021-10-31 21:46:31 +0530

    begonia: Kang AIDL vibrator service from Ronin
    * From Nokia/Ronin_00WW/RNN_sprout:11/RP1A.200720.011/00WW_1_100:user/release-keys
    
    Change-Id: Ia0df504cd29fa240119308763f7a3234af0f7b01

--- device.mk
+++ device.mk
@@ -314,10 +314,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service \
-    android.hardware.vibrator@1.1.vendor \
-    android.hardware.vibrator@1.2.vendor
+    android.hardware.vibrator-V1-ndk_platform.vendor
 
 # VNDK
 PRODUCT_COPY_FILES += \
