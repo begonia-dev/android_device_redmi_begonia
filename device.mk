baa2e418b776530a62f9f8a211fee3720bcc18ad
SahilSonar <sss.sonar2003@gmail.com>
2021-10-31 19:30:14 +0100

    begonia: Switch to AOSP keymaster service
    beanpod generates linker error on android-12.0.0:
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.keymaster@4.0-service.beanpod": cannot locate symbol "_ZN9keymaster19GenerateKeyResponseD1Ev" referenced by "/vendor/bin/hw/android.hardware.keymaster@4.0-service.beanpod"...

--- device.mk
+++ device.mk
@@ -138,6 +138,7 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.0-service \
     android.hardware.keymaster@4.0.vendor \
     libkeymaster4.vendor:64 \
     libkeymaster4support.vendor:64 \
