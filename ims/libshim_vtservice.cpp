6a81616420c61bdaae1b928cc7dddbf7fe2e7898
ZIDAN44 <zidan.roking@gmail.com>
2021-11-13 08:55:17 +0100

    begonia: ims: Add more shims

--- ims/libshim_vtservice.cpp
+++ ims/libshim_vtservice.cpp
@@ -1,3 +1,5 @@
 extern "C" {
    void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_output_flags_tPFviPvS4_ES4_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tjiPK18audio_attributes_tbfi() {}
+   void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb() {}
+   void _ZN7android10MediaMuxerC1EiNS0_12OutputFormatE() {}
 }
