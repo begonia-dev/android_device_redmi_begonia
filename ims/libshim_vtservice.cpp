cb1738bb0026ab756eca31850685c8a92ec041ad
Arian <arian.kulmer@web.de>
2021-12-26 20:02:09 +0200

    begonia: ims: Properly shim libmtk_vt_service.so
    As pointed out by @ R0rt1z2, the previous shims used to break ViLTE
    functionality, so replace them with the proper ones
    
    Originally from
    * https://github.com/realme-mt6785-devs/android_device_realme_RM6785/blob/lineage-19.0/libshims
    * https://github.com/realme-mt6785-devs/android_device_realme_RM6785/blob/lineage-19.0/libshims/libshim_vtservice.cpp
    
    Co-authored-by: SamarV-121 <samarvispute121@pm.me>
    Co-authored-by: R0rt1z2 <rogerortizleal@gmail.com>
    Co-authored-by: Kyle Harrison <khwebmail@gmail.com>
    Signed-off-by: LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
    Change-Id: I109db7220b47e2d6b174bfd176edf6e7449324aa

--- ims/libshim_vtservice.cpp
+++ ims/libshim_vtservice.cpp
@@ -1,5 +1,45 @@
+#include <gui/SurfaceComposerClient.h>
+#include <media/AudioTrack.h>
+
+using namespace android;
+
 extern "C" {
-   void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_output_flags_tPFviPvS4_ES4_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tjiPK18audio_attributes_tbfi() {}
-   void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb() {}
-   void _ZN7android10MediaMuxerC1EiNS0_12OutputFormatE() {}
+void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_output_flags_tPFviPvS4_ES4_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tjiPK18audio_attributes_tbfi(
+        audio_stream_type_t streamType,
+        uint32_t sampleRate,
+        audio_format_t format,
+        audio_channel_mask_t channelMask,
+        size_t frameCount,
+        audio_output_flags_t flags,
+        android::AudioTrack::callback_t cbf,
+        void* user,
+        int32_t notificationFrames,
+        audio_session_t sessionId,
+        android::AudioTrack::transfer_type transferType,
+        const audio_offload_info_t *offloadInfo,
+        uid_t uid,
+        pid_t pid,
+        const audio_attributes_t* pAttributes,
+        bool doNotReconnect,
+        float maxRequiredSpeed,
+        audio_port_handle_t selectedDeviceId) {
+    const AttributionSourceState& attributionSource = AttributionSourceState();
+    new android::AudioTrack(streamType, sampleRate, format, channelMask, frameCount, flags, cbf,
+                            user, notificationFrames, sessionId, transferType, offloadInfo, attributionSource,
+                            pAttributes, doNotReconnect, maxRequiredSpeed, selectedDeviceId);
+        }
+
+void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(
+    void* thisptr, const sp<IGraphicBufferProducer>& bufferProducer, bool controlledByApp, const sp<IBinder>& surfaceControlHandle);
+
+void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb(
+    void* thisptr, const sp<IGraphicBufferProducer> &bufferProducer, bool controlledByApp) {
+        _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(thisptr, bufferProducer, controlledByApp, nullptr);
+}
+
+void _ZN7android11AudioSourceC1E14audio_source_tRKNS_8String16Ejjjjii28audio_microphone_direction_tf(audio_source_t inputSource, const String16 &opPackageName,
+    uint32_t sampleRate, uint32_t channels, uint32_t outSampleRate, uid_t uid, pid_t pid,
+    audio_port_handle_t selectedDeviceId, int32_t selectedMicDirection, float selectedMicFieldDimension);
+
+int _ZN7android10MediaMuxerC1EiNS0_12OutputFormatE[6] = { 0, 1, 2, 3, 4, NULL };
 }
