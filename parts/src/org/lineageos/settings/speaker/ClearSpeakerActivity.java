da7e947c0c337a3a81b5580e93a16eb212f9477f
TheScarastic <warabhishek@gmail.com>
2022-01-06 18:22:32 +0530

    begonia: parts: Adapt Clear Speakers to S style
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>

--- parts/src/org/lineageos/settings/speaker/ClearSpeakerActivity.java
+++ parts/src/org/lineageos/settings/speaker/ClearSpeakerActivity.java
@@ -21,19 +21,22 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-public class ClearSpeakerActivity extends Activity {
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.collapsingtoolbar.R;
+
+public class ClearSpeakerActivity extends CollapsingToolbarBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
-        Fragment fragment = getFragmentManager().findFragmentById(android.R.id.content);
+        Fragment fragment = getFragmentManager().findFragmentById(R.id.content_frame);
         ClearSpeakerFragment clearSpeakerFragment;
         if (fragment == null) {
             clearSpeakerFragment = new ClearSpeakerFragment();
             getFragmentManager().beginTransaction()
-                    .add(android.R.id.content, clearSpeakerFragment)
+                    .add(R.id.content_frame, clearSpeakerFragment)
                     .commit();
         }
     }
