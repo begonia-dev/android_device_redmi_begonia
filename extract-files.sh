a45f8a1a29d636f3c4c35491a35082dbd18c7bb7
TheMalachite <eliasgheeraert@gmail.com>
2022-03-06 13:11:24 +0100

    begonia: Patchelf AIDL and HIDL power service to load ndk backend instead of ndk_platform backend
    Change-Id: I2e9b8d1484d5f533558b4fc166cec4069eb15197
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -60,6 +60,15 @@ function blob_fixup {
         vendor/bin/hw/android.hardware.lights-service.mediatek)
             "${PATCHELF}" --replace-needed "android.hardware.light-V1-ndk_platform.so" "android.hardware.light-V1-ndk.so" "${2}"
             ;;
+        vendor/bin/hw/power_native_test_v_1_1)
+            "${PATCHELF}" --replace-needed "android.hardware.power-V1-ndk_platform.so" "android.hardware.power-V1-ndk.so" "${2}"
+            ;;
+        vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service)
+            "${PATCHELF}" --replace-needed "android.hardware.power-V1-ndk_platform.so" "android.hardware.power-V1-ndk.so" "${2}"
+            ;;
+        vendor/lib64/android.hardware.power-service-mediatek.so)
+            "${PATCHELF}" --replace-needed "android.hardware.power-V1-ndk_platform.so" "android.hardware.power-V1-ndk.so" "${2}"
+            ;;
         vendor/lib64/libudf.so)
             "${PATCHELF}" --replace-needed "libunwindstack.so" "libunwindstack-v30.so" "${2}"
             ;;
