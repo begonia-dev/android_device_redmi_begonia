b0cb57bbe6d09b0b1c6a06012d899a621c1298f0
Georg Veichtlbauer <georg@vware.at>
2022-03-29 11:26:37 +0200

    begonia: Remove CLANG compile flag
    Defaults to true now project-wide.
    
    Change-Id: I1e0ff5adadc889d81c90c816457a72d2621e4b5a
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,6 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_BOOTIMG_HEADER_VERSION := 2
 TARGET_KERNEL_SOURCE := kernel/xiaomi/mt6785
 TARGET_KERNEL_CONFIG := begonia_user_defconfig
-TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 TARGET_KERNEL_ADDITIONAL_FLAGS := HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
