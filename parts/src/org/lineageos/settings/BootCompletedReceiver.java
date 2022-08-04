0a78ac72fec00ea70c36d3d2ccbb4d0e913239a6
7Soldier <reg.fm4@gmail.com>
2022-08-05 01:23:50 +0300

    [SQUASH] begonia: parts: Nuke xiaomi doze parts implementation
    Seems, for legacy devices, this method of dozing not working.
    We even can't pick proximity and pickup sensors from manager.
    Better to drop, until not rework it.
    
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,7 +23,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.util.Log;
 
-import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -34,7 +33,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
-        DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
     }
 }
