d36086ff89c234e7761c619128c3e1d30343a6ff
DarkJoker360 <simoespo159@gmail.com>
2022-08-22 16:45:45 +0200

    begonia: Build android.frameworks.sensorservice@1.0
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- device.mk
+++ device.mk
@@ -297,7 +297,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0.vendor
+    android.hardware.sensors@2.0.vendor \
+    android.frameworks.sensorservice@1.0
 
 PRODUCT_PACKAGES += \
     libsensorndkbridge
