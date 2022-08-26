c6034a5c5b4c7d4aa3226dfe35972d9e026217be
Robert Shih <robertshih@google.com>
2022-08-26 11:17:37 +0200

    begonia: Enable AIDL Clearkey HAL
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- device.mk
+++ device.mk
@@ -105,8 +105,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.0-impl:64 \
-    android.hardware.drm@1.0-service-lazy \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # FM Radio
 PRODUCT_PACKAGES += \
