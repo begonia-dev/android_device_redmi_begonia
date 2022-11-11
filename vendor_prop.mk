2b835e955dca90ea88b06c02f70068075da0464e
Ady Abraham <adyabr@google.com>
2022-11-11 11:09:51 +0100

    begonia: Enable GL comp backpressure
    Enable backpressure for client composition to avoid jank due to
    HWC queue stuffing.
    
    Test: 1. Open Camera
          2. Swipe horizontally to go back to previous app.
    
    Bug: 138083790
    Change-Id: I3d26aa470c1660b23bd97133f39858f92638aca8
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -32,6 +32,7 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
     debug.mtk_tflite.target_nnapi=29 \
     debug.renderengine.backend=skiaglthreaded \
     debug.sf.disable_client_composition_cache=1 \
+    debug.sf.enable_gl_backpressure=1 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.early.app.duration=20000000 \
     debug.sf.early.sf.duration=27600000 \
