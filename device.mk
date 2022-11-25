fb55affe40b6ea310e844162454a69fefc49f3ea
decoder <toniron208@gmail.com>
2022-11-25 04:30:52 +0300

    force push

--- device.mk
+++ device.mk
@@ -202,7 +202,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
