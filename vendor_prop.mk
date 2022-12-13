536904f0c1b80d05eae11a3a9c9807153e7c63b6
Jun Wang <quic_junwan@quicinc.com>
2022-12-13 23:28:06 +0100

    begonia: props: Disable Skia tracing by default
    If the prop "debug.hwui.skia_atrace_enabled" is enabled, too many
    skia tracing calls will be printed in every frame. This has obvious
    overhead on performance/power, and can cause many janks. So disable
    it by default, and user can set it manually if necessary.
    
    Change-Id: I5d470b22a643634bdd7db9f307c29deae6ff176f
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -31,6 +31,7 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
     debug.mediatek.game_pq_enable=1 \
     debug.mtk_tflite.target_nnapi=29 \
     debug.renderengine.backend=skiaglthreaded \
+    debug.hwui.skia_atrace_enabled=false \
     debug.sf.disable_client_composition_cache=1 \
     debug.sf.enable_gl_backpressure=1 \
     debug.sf.latch_unsignaled=1 \
