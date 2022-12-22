fc42794cff71ca2a7b497325cf8f6e8c3db4d545
bengris32 <bengris32@protonmail.ch>
2022-12-22 11:44:17 +0100

    begonia: Drop MediaTek PowerHAL
    * We still have to keep some vendor interfaces and libs
      since some HALs have dependencies on them.
    * Also switch to stub AIDL power service until we can get
      libperfmgr properly implemented.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- device.mk
+++ device.mk
@@ -238,15 +238,10 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power-service.example \
     android.hardware.power@1.0.vendor \
     android.hardware.power@1.1.vendor \
-    android.hardware.power@1.3.vendor \
-    android.hardware.power-V1-ndk.vendor
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/power/power_app_cfg.xml:$(TARGET_COPY_OUT_VENDOR)/etc/power_app_cfg.xml \
-    $(LOCAL_PATH)/configs/power/powercontable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powercontable.xml \
-    $(LOCAL_PATH)/configs/power/powerscntbl.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerscntbl.xml
+    android.hardware.power@1.3.vendor
 
 # MTK In-Call volumes adjusting service
 PRODUCT_PACKAGES += \
