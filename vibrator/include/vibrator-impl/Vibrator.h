05801c02ff282a031d4742b9aa9e4ee69737f4e6
DarkJoker360 <simoespo159@gmail.com>
2022-12-28 16:06:57 +0100

    Revert "[SQUASH] begonia: vibrator: Add support for vibration strengths"
    * Doesn't play well on lower/higher intensities.
    
    This reverts commit 2251a2cbff107205a4214f742b3e998224f10b48.
    
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- vibrator/include/vibrator-impl/Vibrator.h
+++ vibrator/include/vibrator-impl/Vibrator.h
@@ -27,7 +27,6 @@ namespace vibrator {
 const std::string kVibratorState    = "/sys/class/leds/vibrator/state";
 const std::string kVibratorDuration = "/sys/class/leds/vibrator/duration";
 const std::string kVibratorActivate = "/sys/class/leds/vibrator/activate";
-const std::string kVibratorStrength = "/sys/kernel/thunderquake_engine/level";
 
 static std::map<Effect, int32_t> vibEffects = {
     { Effect::CLICK, 50 },
@@ -35,15 +34,8 @@ static std::map<Effect, int32_t> vibEffects = {
     { Effect::TICK, 32 }
 };
 
-static std::map<EffectStrength, int32_t> vibStrengths = {
-    { EffectStrength::LIGHT, 0},
-    { EffectStrength::MEDIUM, 4},
-    { EffectStrength::STRONG, 9}
-};
-
 class Vibrator : public BnVibrator {
 public:
-    Vibrator();
     ndk::ScopedAStatus getCapabilities(int32_t* _aidl_return) override;
     ndk::ScopedAStatus off() override;
     ndk::ScopedAStatus on(int32_t timeoutMs,
@@ -78,10 +70,7 @@ public:
 private:
     static ndk::ScopedAStatus setNode(const std::string path, const std::string value);
     static ndk::ScopedAStatus setNode(const std::string path, const int32_t value);
-    static bool nodeExists(const std::string path);
-
     ndk::ScopedAStatus activate(const int32_t timeoutMs);
-    bool mVibratorStrengthSupported;
 };
 
 }  // namespace vibrator
