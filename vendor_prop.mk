d6a90d1b8e43adb0f0160d7ccf59f34e38f66d45
DarkJoker360 <simoespo159@gmail.com>
2023-01-09 00:13:42 -0800

    begonia: Add zram writeback props
    * This ensures writeback to work properly.
    
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -468,7 +468,10 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
     wifi.concurrent.interface= \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
-    persist.dbg.wfc_avail_ovr=1
+    persist.dbg.wfc_avail_ovr=1 \
+    ro.zram.mark_idle_delay_mins=60 \
+    ro.zram.first_wb_delay_mins=1440 \
+    ro.zram.periodic_wb_delay_hours=2
 
 # Colors
 PRODUCT_PROPERTY_OVERRIDES += \
