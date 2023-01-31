b4ff73f81c40218f92646aabd5c572476b98f4c4
DarkJoker360 <simoespo159@gmail.com>
2023-01-31 10:30:44 +0100

    Revert "begonia: Switch to skiaglthreaded renderthread backend"
    This reverts commit d18dcb5b8213a88ff6ec93ec422416ccc38cc458.
    
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -30,7 +30,6 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
     debug.mediatek.disp_decompress=1 \
     debug.mediatek.game_pq_enable=1 \
     debug.mtk_tflite.target_nnapi=29 \
-    debug.renderengine.backend=skiaglthreaded \
     debug.hwui.skia_atrace_enabled=false \
     debug.sf.disable_client_composition_cache=1 \
     debug.sf.enable_gl_backpressure=1 \
