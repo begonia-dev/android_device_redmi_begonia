674245a8209d97be9b13c38ec099559c8d079320
bengris32 <bengris32@protonmail.ch>
2023-02-25 19:08:12 +0300

    begonia: Build `BesLoudness` app
    * Exposes BesLoudness toggle to user.
    * Need this commit on hardware/mediatek: https://github.com/begonia-dev/android_hardware_mediatek
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I1b2521ce8b469dacb9f874c25acbc290ecc0d92a
    Signed-off-by: MrErenK <akbaseren4751@gmail.com>
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- device.mk
+++ device.mk
@@ -57,6 +57,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# BesLoudness
+PRODUCT_PACKAGES += \
+    BesLoudness
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.3.vendor \
