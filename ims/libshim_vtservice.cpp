fd13126737d43f1624aab7866d7c1491546bed2d
bengris32 <bengris32@protonmail.ch>
2023-04-19 10:58:11 +0200

    begonia: libshim_vtservice: Use AttributionSourceState properly
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- ims/libshim_vtservice.cpp
+++ ims/libshim_vtservice.cpp
@@ -49,6 +49,7 @@ void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_o
         AttributionSourceState* attributionSource = new AttributionSourceState();
         attributionSource->uid = uid;
         attributionSource->pid = pid;
+        attributionSource->packageName = "com.mediatek.ims";
         _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_t20audio_channel_mask_tj20audio_output_flags_tPFviPvS5_ES5_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tRKNS_7content22AttributionSourceStateEPK18audio_attributes_tbfi(
                             thisptr, streamType, sampleRate, format, channelMask, frameCount, flags, cbf,
                             user, notificationFrames, sessionId, transferType, offloadInfo,
