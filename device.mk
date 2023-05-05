6fc8479acd1841440f89dee2c10754e6693c3a6b
Tim Zimmermann <tim@linux4.de>
2023-05-05 12:22:35 +0200

    begonia: Switch to new AIDL memtrack HAL
    Change-Id: I3ae077b5f59e4fcf32489eef64bce8551d758998
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- device.mk
+++ device.mk
@@ -84,9 +84,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.composer@2.1-resources \
     android.hardware.graphics.composer@2.1-resources.vendor \
-    android.hardware.memtrack@1.0.vendor \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
+    android.hardware.memtrack-service.mediatek-mali \
     libdrm.vendor \
     libvulkan \
     libfmq.vendor \
