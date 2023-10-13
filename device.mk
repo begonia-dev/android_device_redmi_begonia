0d86e230037f1028eb02a9009e7a57d38f8a22f7
bengris32 <bengris32@protonmail.ch>
2023-10-13 20:06:54 +0200

    begonia: Switch to AOSP hostapd service
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I1f634de16e59f194784ac5f2863c1a00de321876

--- device.mk
+++ device.mk
@@ -352,15 +352,13 @@ PRODUCT_COPY_FILES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
+    hostapd \
     android.hardware.wifi@1.0.vendor \
     android.hardware.wifi@1.1.vendor \
     android.hardware.wifi@1.2.vendor \
     android.hardware.wifi@1.3.vendor \
     android.hardware.wifi@1.4.vendor \
     android.hardware.wifi@1.5.vendor \
-    android.hardware.wifi.hostapd@1.0.vendor \
-    android.hardware.wifi.hostapd@1.1.vendor \
-    android.hardware.wifi.hostapd@1.2.vendor \
     android.hardware.wifi.offload@1.0.vendor \
     android.hardware.wifi.supplicant@1.0.vendor \
     android.hardware.wifi.supplicant@1.1.vendor \
