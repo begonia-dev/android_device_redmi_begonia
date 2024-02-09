fd8e45a02b20057f5081619f7e7a2bfd5f936be9
DarkJoker360 <simoespo159@gmail.com>
2024-02-09 08:55:54 +0100

    begonia: Symlink needed libs for engineer mode
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- Android.mk
+++ Android.mk
@@ -6,6 +6,14 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
+ENGMODE_LIBS := libem_support_jni.so libjni_shim.so
+ENGMODE_SYMLINKS := $(addprefix $(TARGET_OUT_APPS_PRIVILEGED)/EngineerMode/lib/arm64/,$(notdir $(ENGMODE_LIBS)))
+$(ENGMODE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EngineerMode libs symlinks: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/lib64/$(notdir $@) $@
+
 VENDOR_SYMLINKS := \
     $(TARGET_OUT_VENDOR)/lib/hw \
     $(TARGET_OUT_VENDOR)/lib64/hw
@@ -20,6 +28,7 @@ $(VENDOR_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@ln -sf /vendor/lib64/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.mt6785.so
 	$(hide) touch $@
 
+ALL_DEFAULT_INSTALLED_MODULES += $(ENGMODE_SYMLINKS)
 ALL_DEFAULT_INSTALLED_MODULES += $(VENDOR_SYMLINKS)
 
 endif
